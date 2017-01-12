d755088bf77b717ad57c940f8ecedc1c8be35692
Chris <mail@christopherparsons.me>
2017-01-12 14:09:42 +0000

    Re-doing JS
    Rewritten JS as my commit got lost

---
+++ twitch.js
@@ -0,0 +1,29 @@
+function getTwitch() {	
+    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/esl_sc2/?callback=?', function (info) {
+        var statusSc;
+        if (info.stream === null) {
+            statusSc = 'Offline';
+        } else if (info.stream === undefined) {
+            statusSc = 'Dead';
+        } else {
+            statusSc = 'Online';
+        }
+        $('#sc-status').text(statusSc);
+        console.log(statusSc);
+    })
+    
+    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/a_seagull/?callback=?', function (info) {
+        var statusSeagull;
+        if (info.stream === null) {
+            statusSeagull = 'Offline';
+        } else if (info.stream === undefined) {
+            statusSeagull = 'Dead';
+        } else {
+            statusSeagull = 'Online';
+        }
+        $('#seagull-status').text(statusSeagull);
+        console.log(statusSeagull);
+    })
+};
+
+getTwitch();
