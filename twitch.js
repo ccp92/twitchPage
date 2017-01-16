8c0eb96e506057e9d0d1f693fb9752c1bcb5d88e
Chris <mail@christopherparsons.me>
2017-01-16 08:16:24 +0000

    Additional Information
    I've updated the js to pull more information from the API.
    Page now displays, the stream's status message, the game being played
    and a preview image of the stream.

--- twitch.js
+++ twitch.js
@@ -1,42 +1,44 @@
 function getTwitch() {	
     $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/esl_sc2', function (info) {
         var statusSc;
+        var gameSc = 'n/a';
         if (info.stream === null) {
             statusSc = 'Offline';
         } else if (info.stream === undefined) {
             statusSc = 'Dead';
         } else {
             statusSc = 'Online';
+            gameSc = info.stream.game;
+            var previewSc = info.stream.preview.medium;
+            var infoSc = info.stream.channel.status;
         }
-        
         console.log('ESL_SC2: ' + statusSc);
-        if (statusSc === 'Online') {
-            gameSc = info.stream['game'];
-        } else {
-            gameSc = 'n/a';
-        }
+        console.log(previewSc);
+        console.log(infoSc);
         
-        $('#sc-status').text(statusSc);
+        $('#sc-status').text(statusSc+': '+infoSc);
         $('#sc-game').text(gameSc);
+        $('#sc-preview').attr('src',previewSc);
     })
     
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/a_seagull/?callback=?', function (info) {
+    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/a_seagull', function (info) {
         var statusSeagull;
+        var gameSeagull = 'n/a';
         if (info.stream === null) {
             statusSeagull = 'Offline';
         } else if (info.stream === undefined) {
             statusSeagull = 'Dead';
         } else {
             statusSeagull = 'Online';
+            gameSeagull = info.stream.game;
+            var previewSeagull = info.stream.preview.medium;
+            var infoSeagull = info.stream.channel.status;
         }
         console.log('A_Seagull: '+statusSeagull);
-        if (statusSeagull === 'Online') {
-            gameSeagull = info.stream['game'];
-        } else {
-            gameSeagull = 'n/a';
-        }
-        $('#seagull-status').text(statusSeagull);
+
+        $('#seagull-status').text(statusSeagull+': '+infoSeagull);
         $('#seagull-game').text(gameSeagull);
+        $('#seagull-preview').attr('src',previewSeagull);
     })
 };
 
@@ -45,21 +47,22 @@ getTwitch();
 function buttonTwitch() {
     var search = document.getElementById('streamer').value;
     console.log(search);
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+search+'/?callback=?', function(info) {
+    $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/'+search, function(info) {
         var status;
+        var game = 'n/a';
         if (info.stream===null) {
             status = 'Offline';
         } else if (info.stream===undefined) {
             status = 'Dead';
         } else {
             status = 'Online';
+            game = info.stream.game;
+            var preview = info.stream.preview.medium;
+            var info = info.stream.channel.status;
         }
-        if (status === 'Online') {
-            game = info.stream['game'];
-        } else {
-            game = 'n/a';
-        }
-        $('#status').text(status);
+
+        $('#status').text(status+': '+info);
         $('#game').text(game);
+        $('#preview').attr('src',preview);
     })
 };
